9b88927bec4b5661d3ee558e77b8509662ffcc71
Hayat <hayatjahangir0@gmail.com>
2024-10-24 19:26:49 +0530

    html for onclick assignment

---
+++ DOM/Onclick.js
@@ -0,0 +1,6 @@
+
+
+// JavaScript to change the heading text on click
+document.getElementById("click").addEventListener("click", function() {
+    document.getElementById("head").textContent = "You Clicked!";
+});

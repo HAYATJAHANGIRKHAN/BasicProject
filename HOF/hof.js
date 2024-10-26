8024c5193d3efec8426ce19af25cd4ea0a80a7c3
Hayat <hayatjahangir0@gmail.com>
2024-10-26 19:55:15 +0530

    it about hof we use nested function

--- HOF/hof.js
+++ HOF/hof.js
@@ -1,21 +1,44 @@
-const powertw0=(n)=>{
-// console.log(powercube**n);
-return n**2;
+// const powertw0=(n)=>{
+// // console.log(powercube**n);
+// return n**2;
 
-}
-function powercube(powertw0,n){
-    return powertw0(n)*n;
-}
+// }
+// function powercube(powertw0,n){
+//     return powertw0(n)*n;
+// }
 
-console.log(powercube(powertw0,3));
+// console.log(powercube(powertw0,3));
 
 
-function sayHello(){
-    return function(){
-        console.log("Asalamulaikum Jaaniya")
+// function sayHello(){
+//     return function(){
+//         console.log("Asalamulaikum Jaaniya")
        
+//     }
+// }
+// let guessValue= sayHello();
+// console.log(guessValue);
+// guessValue();
+
+const highOrder = n =>{
+    const Onefun = m =>{
+    const twofun = P => {
+     return  n+m+P;
     }
+    return twofun;
+    }
+    return Onefun;
 }
-let guessValue= sayHello();
-console.log(guessValue);
-guessValue();
+  console.log(highOrder(2)(3)(4)); 
+
+  const a = x =>{
+  const b = y =>{
+  const c = z =>{
+    return x+y*z;
+
+    }
+    return c;
+  }
+  return b;
+};
+console.log(a(5)(6)(9));

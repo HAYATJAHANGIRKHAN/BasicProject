d72b4df14cbac14be9b0460436f18ae4dd511744
Hayat <hayatjahangir0@gmail.com>
2024-10-26 20:23:51 +0530

    here we know how to acutually  hof  there on small create and also set interval of time and settime out  how to us both

--- HOF/hof.js
+++ HOF/hof.js
@@ -20,25 +20,45 @@
 // console.log(guessValue);
 // guessValue();
 
-const highOrder = n =>{
-    const Onefun = m =>{
-    const twofun = P => {
-     return  n+m+P;
-    }
-    return twofun;
-    }
-    return Onefun;
+// const highOrder = n =>{
+//     const Onefun = m =>{
+//     const twofun = P => {
+//      return  n+m+P;
+//     }
+//     return twofun;
+//     }
+//     return Onefun;
+// }
+//   console.log(highOrder(2)(3)(4)); 
+
+//   const a = x =>{
+//   const b = y =>{
+//   const c = z =>{
+//     return x+y*z;
+
+//     }
+//     return c;
+//   }
+//   return b;
+// };
+// console.log(a(5)(6)(9));
+
+
+const myNumber= [1,2,3,4,5]
+
+const SumArray= arry => {
+    let total =0;
+    arry.forEach(function(element){
+        total += element
+    });
+    return total;
 }
-  console.log(highOrder(2)(3)(4)); 
 
-  const a = x =>{
-  const b = y =>{
-  const c = z =>{
-    return x+y*z;
+console.log(SumArray(myNumber));
 
-    }
-    return c;
-  }
-  return b;
-};
-console.log(a(5)(6)(9));
+
+function tricolbal(){
+    console.log("Asalamualaikum");
+}
+// setInterval(tricolbal,2000);
+setTimeout (tricolbal,5000);

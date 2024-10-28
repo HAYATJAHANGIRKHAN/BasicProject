1f4b723cb69f1d6f7e6553132846840e94509182
Hayat <hayatjahangir0@gmail.com>
2024-10-28 21:32:57 +0530

    class eg

--- class_objects/clobj.js
+++ class_objects/clobj.js
@@ -4,6 +4,11 @@ class product{
    rating;
         //behaviours -> function -> member function
 display(){
-    console.log("display")
+    console.log("display");
 }
 }
+
+const p =  new product();
+console.log(p);
+
+p.display();

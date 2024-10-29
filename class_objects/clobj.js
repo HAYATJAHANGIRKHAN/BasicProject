148f9f5042af3ba9a0ac2fcb3c7d4a7753bf9430
Hayat <hayatjahangir0@gmail.com>
2024-10-29 21:22:34 +0530

    constructor in function

--- class_objects/clobj.js
+++ class_objects/clobj.js
@@ -22,20 +22,45 @@
 // console.log(p);
 
 // p.display();
-class product{
+// class product{
 
-constructor(naam,price,rating){
-    console.log("calling current",this);
-    this.rating= rating;
-this.naam= naam;
-this.price= price;
-}
+// constructor(naam,price,rating){
+//     console.log("calling current",this);
+//     this.rating= rating;
+// this.naam= naam;
+// this.price= price;
+// }
 
-display(){
-    console.log("this refer to ",this);
-}
-}
+// display(){
+//     console.log("this refer to ",this);
+// }
+// }
 
-const hay= new product("redmi",3000,3)
-    console.log(hay)
-    hay.display()
+// const hay= new product("redmi",3000,3)
+//     console.log(hay)
+//     hay.display()
+
+// constructor in function
+function product(n,p,r){
+    this.name = n;
+    this.price = p;
+    this.rating = r;
+}
+const p= new product ("jaaniya",100000,5);
+console.log(p);
+
+
+
+
+
+
+
+    /**
+     * 1. this keyword in js is different fromthan other languages.
+     *2.this keyword refers to the  context from where we called.
+
+     in the function constructor also 
+     -if u return primitive it is ignored  ansd we return the object reffered  by this.
+     -if u return  a custom obj ,then the custom obj is returned.
+     - if u do not return anything , then object reffered by this is    returned.
+     */

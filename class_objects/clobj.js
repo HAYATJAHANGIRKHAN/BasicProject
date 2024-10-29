bb083f270f55967639150768c8352c4cdac498d4
Hayat <hayatjahangir0@gmail.com>
2024-10-29 20:38:48 +0530

    here we create a constructor and how to use of class

--- class_objects/clobj.js
+++ class_objects/clobj.js
@@ -1,14 +1,41 @@
+// class product{
+// //    name;
+// //    price;
+// //    rating;
+//         //behaviours -> function -> member function
+
+// constructor(name, price, rating) { 
+//     console.log("calling the current product");
+//     this.name = name;
+//     this.price = price;
+//     this.rating = rating;
+
+// }
+
+// display(){
+//     console.log("display");
+// }
+// }
+
+// const p =  new product("mi",22000,4);// new--> create an empty plain object
+// //--> in the above piease of code we are calling the constructor method
+// console.log(p);
+
+// p.display();
 class product{
-   name;
-   price;
-   rating;
-        //behaviours -> function -> member function
+
+constructor(naam,price,rating){
+    console.log("calling current");
+    this.rating= rating;
+this.naam= naam;
+this.price= price;
+}
+
 display(){
     console.log("display");
 }
 }
 
-const p =  new product();
-console.log(p);
-
-p.display();
+const hay= new product("redmi",3000,3)
+    console.log(hay)
+    hay.display()

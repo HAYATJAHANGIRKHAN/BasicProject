5595a3bc4b4d08aa867fbdbad00b297200cc28a7
Hayat <hayatjahangir0@gmail.com>
2024-11-02 19:23:58 +0530

    add some more examples and settimeout

--- HOF/callback.js
+++ HOF/callback.js
@@ -2,8 +2,18 @@ function h(x,fn){
 //h-> is a HOF
 //fn-> is a callback
 console.log(x*x)
-fn()
+fn(x*x)
 }
-h(10,function(){
-    console.log("call back done")
-})
+// h(10,function(){
+//     console.log("call back done")
+// })
+
+h(10,exec);
+function exec (n){
+    console.log("squared value is ", n);
+}
+
+setTimeout (function hh(){
+    console.log("time done after 5s")
+},5000)
+
